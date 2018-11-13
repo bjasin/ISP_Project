999e58dcb4d095321bf1865a46b7548e57a7cc09
Jordan <jordanking@mymail.mines.edu>
2018-11-13 16:41:02 -0700

    Changed file directories....

--- php/create_account/pix_selection.php
+++ php/create_account/pix_selection.php
@@ -2,7 +2,7 @@
 session_start();
 ?>
 <?php
-$target_dir = "uploads/";
+$target_dir = "../uploads/";
 $target_file = $target_dir . basename($_FILES["upload"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
